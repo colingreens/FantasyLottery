3425af04f0158dbfd816b659e5173c037df5de61
cbenson <cbenson@idtdna.com>
2021-08-20 15:38:29 -0700

    Moved scripts to own folder

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function(){
-    document.body.classList.toggle('dark-theme')
-
-    console.log('current class name:' + className)
-})
