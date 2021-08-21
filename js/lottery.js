f68542f4f357bccbe4a95597ccfc13dd9994934f
Colin Benson <colin.j.benson@gmail.com>
2021-08-20 17:33:18 -0700

    Set up the draft!

--- js/lottery.js
+++ js/lottery.js
@@ -1,19 +1,7 @@
 'use strict'
 
-function Lottery(){
-    let teamPool = [];
-    let lotteryPool = [];
-
-    teamPool.push({name:"TeamOne", odds:25});
-    teamPool.push({name:"TeamTwo", odds:25});
-    teamPool.push({name:"TeamThree", odds:25});
-    teamPool.push({name:"TeamFour", odds:10});
-    teamPool.push({name:"TeamFive", odds:10});
-    teamPool.push({name:"TeamSix", odds:5});
-
-    teamPool.forEach( item => Populate(item));
-    LotteryTime(lotteryPool);   
-}
+var resultsTable = document.getElementById("resultsTable");
+var lotteryPool = [];
 
 function Populate(item){
 
@@ -27,9 +15,37 @@ function GetRandomInt(max){
 }
 
 function LotteryTime(lotteryPool){
+    var i = 0;
     while (lotteryPool.length > 0) {
         var selectedTeam = lotteryPool[GetRandomInt(lotteryPool.length)];
-        console.log("Selected Pick Goes to "+ selectedTeam.name);
+        i++;
+        var row = resultsTable.insertRow(i);
+        row.insertCell(0).innerHTML = i;
+        row.insertCell(1).innerHTML = selectedTeam.name;
         lotteryPool = lotteryPool.filter(item => item.name != selectedTeam.name);
     }
 }
+
+function buildDraftResultsTable(){
+
+    var teamPool = [];     
+    var teamTable = document.getElementById("teamTable");
+
+    var teamCount = teamTable.rows.length -1;
+
+    var header = resultsTable.insertRow(0);
+    header.insertCell(0).innerHTML = "Pick";
+    header.insertCell(1).innerHTML = "Team";
+
+    for (let i = 0; i < teamCount; i++) {
+        
+        var teamName = teamTable.rows[i+1].cells.item(0).children[0].value;
+        var teamOdds = teamTable.rows[i+1].cells.item(1).children[0].value;
+        teamPool.push({name:teamName, odds:teamOdds});;           
+    }
+
+    teamPool.forEach( item => Populate(item));
+    LotteryTime(lotteryPool);
+  
+  
+  }
