57f2712b7034ca493ed5c5acccae4bf8e8e37f13
Colin Benson <colin.j.benson@gmail.com>
2022-01-02 12:44:06 -0800

    shows rows

--- js/lottery.js
+++ js/lottery.js
@@ -17,17 +17,33 @@ function GetRandomInt(max){
 
 function LotteryTime(lotteryPool){
     var header = resultsTableHead.insertRow();
-    header.insertCell().innerHTML = "Pick";
-    header.insertCell().innerHTML = "Team";
+    header.insertCell().innerHTML = "Pick Number";
+    header.insertCell().innerHTML = "Team Name";
     
     var i = 0;
     while (lotteryPool.length > 0) {
         var selectedTeam = lotteryPool[GetRandomInt(lotteryPool.length)];
         i++;
         var row = resultsTableBody.insertRow();
+        row.style.display = 'none';
         row.insertCell(0).innerHTML = i;
         row.insertCell(1).innerHTML = selectedTeam.name;
         lotteryPool = lotteryPool.filter(item => item.name != selectedTeam.name);
+    }
+}
+
+function showLastPick(){
+    var resultsTable = document.getElementById("resultsTable");
+    var teamCount = resultsTable.rows.length -1;
+
+    for (let i = teamCount; i >= 0; i--) {
+        var lastRow = resultsTable.rows[i];
+        
+        if(lastRow.style.display === 'none'){
+            alert(lastRow.cells.item(0).innerHTML)           
+            lastRow.style.display = "";
+            return;
+        }                   
     }
 }
 
